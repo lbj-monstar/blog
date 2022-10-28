24fedb43a5a571e7cc949c6f838860a55b867a4f
lbj <luofatso@gmail.com>
2022-10-28 10:26:29 +0800

    fix: 修改配置

--- deploy.sh
+++ deploy.sh
@@ -7,13 +7,12 @@ set -e
 npm run build
 
 # 进入生成的文件夹
-cd docs/.vuepress/dist
+cd docs/.vitepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:lbj-monstar/blog.git master:gh-pages
-# 例如 git push -f git@github.com:koala-coding/blog.git master:gh-pages 
+git push -f git@github.com:lbj-monstar/blog.git master:gh-pages1
 cd -
